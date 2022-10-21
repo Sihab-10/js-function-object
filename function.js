a5817beb3b506fe9207a91f88761dffffdb51d55
Sihab-10 <sihabsarar10@gmail.com>
2022-10-21 10:52:03 +0600

    start journey with js-function

---
+++ function.js
@@ -0,0 +1,8 @@
+// function declaration
+function startFan() {
+  console.log("Stand up");
+  console.log("walks towards the switch");
+  console.log("Press the switch");
+}
+// function call / invocation
+startFan();
