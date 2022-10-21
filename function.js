d82568694b9b25d9f7a1274efe77fe3224fdb51b
Sihab-10 <sihabsarar10@gmail.com>
2022-10-21 10:57:48 +0600

    fuction added

--- function.js
+++ function.js
@@ -5,4 +5,12 @@ function startFan() {
   console.log("Press the switch");
 }
 // function call / invocation
+// startFan();
+// bar bar function calling
+startFan();
+console.log("---------------hello--------------");
+startFan();
+console.log("---------------this is-------------");
+startFan();
+console.log("--------------Function----------------");
 startFan();
