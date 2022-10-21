83e91cd9ea0a68003dc6025a3992dd542b07779f
Sihab-10 <sihabsarar10@gmail.com>
2022-10-21 11:42:02 +0600

    parameter explained

--- parameter.js
+++ parameter.js
@@ -1,8 +1,25 @@
+// function bringSingara(money) {
+//   console.log("Mama singara den");
+//   console.log(money);
+// }
+// bringSingara(200);
+// bringSingara(300);
+// bringSingara(400);
+// bringSingara(500);
+
+// this is the one type of parameter declaration on display output from parameter
+/* 
 function bringSingara(money) {
-  console.log("Mama singara den");
   console.log(money);
+  console.log("ai nen singara");
 }
-bringSingara(200);
-bringSingara(300);
-bringSingara(400);
-bringSingara(500);
+var taka = 500;
+bringSingara(taka);
+
+*/
+function add(num1, num2, addition) {
+  console.log("going to add num1 and num2:", num1, "and", num2);
+  var addition = num1 + num2;
+  console.log("Addition of num1 and num2 is: ", addition);
+}
+add(2, 3);
