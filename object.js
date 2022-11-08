22d6a0309523c3a98756831ccedb7e081e9a4b4b
Sihab-10 <sihabsarar10@gmail.com>
2022-11-08 11:29:51 +0600

    basic of object in js added

---
+++ object.js
@@ -0,0 +1,25 @@
+// object হল একটি variable যা অনেক মান ধারন করতে পারে।
+var student = {
+  id: 115,
+  name: "Solaiman Khan",
+  class: 9,
+  marks: 98,
+};
+
+var mobile = {
+  brand: "Samsung",
+  price: 19000,
+  storage: "64gb",
+  camera: "7MP",
+};
+
+var myComputer = {
+  brand: "lenovo",
+  price: 39000,
+  color: "Silver",
+  processor: "i7",
+};
+
+console.log(myComputer);
+myComputer.processor = "i79";
+console.log(myComputer);
